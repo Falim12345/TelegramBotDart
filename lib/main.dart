3a018d718c454fab716ceec9a33f8383926dfcf5
Lasenko Pavel <pal.2323zet@gmail.com>
2024-02-25 23:30:59 +0200

    Made saving bot message ID

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:convert';
 
 import 'package:dart_application_1/model/trading_pair.dart';
@@ -26,50 +25,69 @@ void main() async {
   final username = (await Telegram(BotUtil.botToken).getMe()).username;
   var teledart = TeleDart(BotUtil.botToken, Event(username!));
   List<int> messages = [];
+  List<int> userMessages = [];
 
   teledart.start();
   // teledart.onMessage().listen((message) {
   //   messages.add(message.messageId);
   // });
 
-  teledart.onCommand('start').listen((event) async {
-    messages.add(event.messageId);
-
-    await event.reply(
+  teledart.onCommand('start').listen((message) async {
+    var ferstReply = await message.reply(
         'Good day, to select the currency pair you want to track, select the command /select \u{1F388}\u{270C} ');
+    ferstReply;
+    messages.add(ferstReply.messageId);
   });
 
-  teledart.onCommand('select').listen((event) async {
+  teledart.onCommand('select').listen((message) async {
+    userMessages.add(message.messageId);
+    for (var element in messages) {
+      print(element);
+    }
+
     var keyboard = coinPairs.map((e) => [KeyboardButton(text: e)]).toList();
+    var messageId = message.messageId;
+    print(messageId);
 
     var replyKeyboard = ReplyKeyboardMarkup(
         keyboard: keyboard,
         resizeKeyboard: true,
         oneTimeKeyboard: true,
         selective: true);
-    await event.reply('Select a trading pair', replyMarkup: replyKeyboard);
+    var secondReply = await message.reply('Select a trading pair',
+        replyMarkup: replyKeyboard);
+    secondReply;
+    messages.add(secondReply.messageId);
   });
 
   teledart.onMessage().listen((message) async {
-    // var userId = message.from?.id;
+    // message.from?.isBot;
+    // print(message.messageId);
+
+    // var userId = message.from?.username;
     // print(userId);
-    var userId = message.from?.username;
-    print(userId);
 
     if (coinPairs.contains(message.text)) {
       userIndexChoice = message.text;
       print('User selected trading pair: ${message.text}');
-      await message.reply('Select upper border');
+      var thirdReply = await message.reply('Select upper border');
+      thirdReply;
+      messages.add(thirdReply.messageId);
       isWaitingForUpperLimit = true;
     } else if (isWaitingForUpperLimit) {
       if (Validation().isValidLimit(message.text ?? 'nonValid')) {
         print('User selected upper limit: ${message.text}');
         upperLimit = message.text;
-        await message.reply('Select Bottom Border');
+        var fourthReply = await message.reply('Select Bottom Border');
+        fourthReply;
+        messages.add(fourthReply.messageId);
         isWaitingForUpperLimit = false;
         isWaitingForLowerLimit = true;
       } else {
-        await message.reply('Upper limit is not valid. Please try again.');
+        var fifthReply =
+            await message.reply('Upper limit is not valid. Please try again.');
+        fifthReply;
+        messages.add(fifthReply.messageId);
       }
     } else if (isWaitingForLowerLimit) {
       lowerLimit = message.text;
@@ -78,19 +96,11 @@ void main() async {
           double.parse(message.text!) != double.parse(upperLimit ?? '0')) {
         lowerLimit = message.text;
         print('User selected lower limit: ${message.text}');
-        await message.reply(
+        var sixthReply = await message.reply(
             'You are now tracking a trading pair $userIndexChoice with an upper $upperLimit and lower $lowerLimit boundary. To start tracking a new one, enter the command /select');
         isWaitingForLowerLimit = false;
-        // for (var element in messages) {
-        //   print(element);
-        // }
-        // if (!message.text!.startsWith('#')) {
-        //   for (var messageId in messages) {
-        //     await Telegram(BotUtil.botToken)
-        //         .deleteMessage(message.chat.id, messageId);
-        //   }
-        // }
-
+        sixthReply;
+        messages.add(sixthReply.messageId);
         final webSocketChannel = await getIndexBinance
             .getWebSocketChannel(userIndexChoice?.toLowerCase() ?? '');
 
@@ -113,16 +123,18 @@ void main() async {
 
           if (price > tradingPair.upperLimit!) {
             print(tradingPair.upperLimit);
-            await message.reply(
+            var seventhReply = await message.reply(
                 '#up\u{1F4C8}\u{2197} $symbol > ${tradingPair.upperLimit}.');
+            seventhReply;
             print('пересекла верхний предел Средняя цена $price');
 
             webSocketChannel.sink.close();
           } else if (price < tradingPair.lowerLimit!) {
             print(tradingPair.lowerLimit.runtimeType);
             print(price.runtimeType);
-            await message.reply(
+            var eightthReply = await message.reply(
                 '#down\u{1F4C9}\u{2198} $symbol < ${tradingPair.lowerLimit}.');
+            eightthReply;
             print('пересекла нижний предел Средняя цена $price');
 
             webSocketChannel.sink.close();
@@ -133,10 +145,16 @@ void main() async {
           print('Connection closed');
         });
       } else {
-        await message.reply('Lower limit is not valid. Please try again.');
+        var ninethReply =
+            await message.reply('Lower limit is not valid. Please try again.');
+        ninethReply;
+        messages.add(ninethReply.messageId);
       }
     } else {
-      await message.reply('Invalid trading pair. Please try again.');
+      var thenthReply =
+          await message.reply('Invalid trading pair. Please try again.');
+      thenthReply;
+      messages.add(thenthReply.messageId);
     }
   });
 
@@ -147,10 +165,14 @@ void main() async {
               'Currency pair ${element.name}, Upper limit: ${element.upperLimit}, Lower limit: ${element.lowerLimit}, Tracking Status: ${element.webSocketChannel?.closeReason == null ? "Connection Open" : "Connection Closed"}')
           .join('\n');
 
-      await event.reply(pairsMessage);
+      var eleventhReply = await event.reply(pairsMessage);
+      eleventhReply;
+      messages.add(eleventhReply.messageId);
     } else {
-      await event
+      var twelfthReply = await event
           .reply('There are no trading pairs selected. Please try again.');
+      twelfthReply;
+      messages.add(twelfthReply.messageId);
     }
   });
 
@@ -165,9 +187,14 @@ void main() async {
               'Currency pair ${pair.name}, Upper limit: ${pair.upperLimit}, Lower limit: ${pair.lowerLimit}, Connection Status: Open')
           .join('\n');
 
-      await event.reply(activePairsMessage);
+      var thirteenthReply = await event.reply(activePairsMessage);
+      thirteenthReply;
+      messages.add(thirteenthReply.messageId);
     } else {
-      await event.reply('There are no active trading pairs. Please try again.');
+      var fourteenthReply = await event
+          .reply('There are no active trading pairs. Please try again.');
+      fourteenthReply;
+      messages.add(fourteenthReply.messageId);
     }
   });
 }
